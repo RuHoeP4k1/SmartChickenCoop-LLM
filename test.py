98b787258e9a347b453fec7f9b01a33c19ce03d9
RuHoeP4k1 <hoevenruben@gmail.com>
2025-12-07 12:44:58 +0100

    different document, new embedding

--- test.py
+++
@@ -1,6 +0,0 @@
-import ollama
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
-
-print("Imports werken!")
--- test_pipeline.py
+++ test_pipeline.py
@@ -1,9 +1,14 @@
 from main import answer_with_realtime_data
 
+import os
+print("Working directory:", os.getcwd())
+print("Exists?", os.path.exists("test_docs"))
+"""
+
 result = answer_with_realtime_data(
     folder_path="test_docs",
     json_path="Data/sensors/sensor_data_1.json",
     user_question="What is the biggest risk for my chickens right now?"
 )
 
-print(result["answer"])
+print(result["answer"])"""
